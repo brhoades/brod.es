4bc19d8a384a9dc629ec1e365ab3bf42113c8f3f
Billy Rhoades <billyarhoades@gmail.com>
2014-08-05 20:05:48 -0500

    Beginning of sinatra-based rewrite.

---
+++ run.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'RedCloth'
+
+set :root, File.expand_path( File.dirname( __FILE__ ) )
+set :public_folder, "./public"
+
+def drawJobs( )
+  jobsf = Dir.glob "jobs/*.textile"
+  jobs = [ ]
+
+  jobsf.each do |j|
+    jobs << ( RedCloth.new File.read j ).to_html
+  end
+
+  jobs.join "\n"
+end 
+
+get '/' do
+  print File.expand_path( File.dirname( $0 ) ), "\n\n"
+  erb :index
+end
+
