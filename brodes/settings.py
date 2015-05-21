762533f288b8d974b9fe786dca2e4ffa8030d677
Billy Rhoades <billyarhoades@gmail.com>
2015-05-21 14:04:26 -0500

    Fixed textile formatting issues.

--- brodes/settings.py
+++ brodes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fontawesome',
+    'django_markwhat',
 )
 
 MIDDLEWARE_CLASSES = (
--- greeter/views.py
+++ greeter/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+import glob
+
 # Create your views here.
 def index(request):
     template = loader.get_template('greeter/index.html')
-    context = RequestContext(request, {})
+    context = {'jobs': []}
+    
+    jobs = glob.glob('static/jobs/*')
+    jobs.reverse() # python globs in ascending
+    for file in jobs:
+        f = open(file, 'r')
+        context['jobs'].append(f.read())
+
+    context = RequestContext(request, context)
     return HttpResponse(template.render(context))
