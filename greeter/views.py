a775cc28535c2d3c208713f53d3bd9325fe3fe2d
Billy Rhoades <billyarhoades@gmail.com>
2015-05-28 14:51:02 -0500

    Added projects section + filler content.

--- greeter/views.py
+++ greeter/views.py
@@ -7,12 +7,17 @@ import glob
 # Create your views here.
 def index(request):
     template = loader.get_template('greeter/index.html')
-    context = {'jobs': []}
+    context = {'jobs': [], 'projects': []}
     
     jobs = sorted(glob.glob('static/greeter/jobs/*'), reverse=True)
     for file in jobs:
         f = open(file, 'r')
         context['jobs'].append(f.read())
+
+    projects = sorted(glob.glob('static/greeter/projects/*'), reverse=True)
+    for file in projects:
+        f = open(file, 'r')
+        context['projects'].append(f.read())
 
     context = RequestContext(request, context)
     return HttpResponse(template.render(context))
