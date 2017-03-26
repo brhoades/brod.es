b52f5dc656336831c34c4a232b538e3afb949643
Billy Rhoades <aaron5367@gmail.com>
2017-03-26 11:41:16 -0500

    Updated icons, removed 5201.

--- greeter/urls.py
+++ greeter/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, url, include
 from greeter import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index),
-    url(r'^5201/?(.*)$', views.cpp),
+    url(r'^$', views.index)
 )
