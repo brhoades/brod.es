35337f982ff6666cafa715364d826551f8896b16
Billy Rhoades <billy.rhoades@cerner.com>
2018-04-08 18:19:37 -0500

    Added aws-upload.

---
+++ aws-upload.conf.js
@@ -0,0 +1,11 @@
+/* tslint:disable */
+module.exports = {
+  credentials: "../aws-credentials.json",
+  bucketName: "brod.es",
+  patterns:[
+    "*.js",
+    "*.png",
+    "*.map",
+    "*.html"
+  ]
+}
