6540ae4a869bdbdce11d4df48ac5e07c5032a75d
Billy Rhoades <billyarhoades@gmail.com>
2019-08-17 11:21:49 -0500

    Updated about me.

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 
 export default () => (
   <React.Fragment>
-    <p>I like programming--- no, seriously, I <em>like</em> programming. I program as a hobby after work and on the weekends. Currently TypeScript is my favorite language, but Python follows as a close second. As can be seen on <a href="https://github.com/brhoades/">GitHub</a>, I’m not afraid of using a new language if it fits a problem better.</p>
+    <p>
+        I like programming--- no, seriously, I <em>like</em> programming. I program as a hobby: both after work and on the weekends. Currently TypeScript is my favorite language, but Python follows as a close second. I am also passionate about good management; both above me and under me. Read <a href="https://blog.brod.es/management/2019/07/29/one-on-ones.html">my blog</a> about 1-on-1s and see how I tick.
+    </p>
 
-      <p>If I were asked to “tell me about yourself” in an interview, I would begin with my hometown. I grew up in a small town of 161, Blue Eye, Missouri. I attended the same, small school for 13 years and graduated in its largest ever class of 76 students.</p>
+    <p>If I were asked to “tell me about yourself” in an interview, I would begin with my hometown. I grew up in a small town of 161, Blue Eye, Missouri and it heavily influenced how I see education and learning. I attended the same, small school for 13 years and graduated in its largest-ever class of 76 students.</p>
 
-      <p>With a small school, I had no opportunity for a introduction into computer science. In 8th grade I was really into RuneScape, but had gotten bored with the repetitive tasks. I joined an online community of people that wrote bots to play the game for them. I learned Pascal, my first programming language, and became addicted. I simultaneously learned PHP while developing an improved website.</p>
+    <p>With a small school, I had no opportunity for a introduction into computer science. In 8th grade, I was really into RuneScape but became bored with actually playing it. I joined an online community that wrote bots to play the game for them; from this, I learned my first programming language, Pascal.</p>
 
-      <p>In High School, I joined a community called Knights of Reason and got involved with the development team for their version of Tremulous. At this point, I had taught myself C, a bit of Bash, and learned how to automate builds and use debugging tools. Using these skills, I then launched my own mod for Tremulous, KoRx, wrote and managed several PHP applications, KoRPS and KoRMS, for Knights of Reason after becoming a co-owner. PHP, Pascal, and C were the bulk of the exposure I had until I entered college, which is cleanly formatted below.</p>
+    <p>
+      In High School, I became co-owner of Knights of Reason: a gaming community centered around a game Tremulous. Over the next 4 years, I learned the majority of my management and people skills from structuring, discipling, and leading a group of 40 individuals around the globe. I built complex tools in PHP / Bash which assisted in this: KoRPS and KoRMS, which enabled democratic voting in / out of members to our group.
+    </p>
 
-      <p>On the other hand, I was fortunate to have discovered Linux in 8th grade. I did not know what to make of it then, but something about it stuck with me. A year later and I had it on my home computer. Fast forward three years and my desktop and laptop both ran Arch Linux, with the latter not even having a Windows install. Today is the same; all my personal machines run some variant of Linux (recently Arch) as the primary OS.</p>
+      <p>I was fortunate to have discovered Linux in 8th grade. I did not know what to make of it then, but something about it stuck with me. A year later and I had it on my home computer. Fast forward three years and my desktop and laptop both ran Arch Linux, with the latter not even having a Windows install. 10+ years later, all my personal machines still run some variant of Linux as the primary OS.</p>
       </React.Fragment>
 );
