3f19d39f39a9f931cba5be6c8de74440a1ce1a80
Billy J Rhoades II <me@brod.es>
2020-08-02 16:57:48 -0700

    slight AboutMe change.

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default () => (
   <React.Fragment>
     <p>
-        I like programming--- no, seriously, I <em>like</em> programming. I program as a hobby: both after work and on the weekends. Currently TypeScript is my favorite language, but Python follows as a close second. I am also passionate about good management; both above me and under me. Read <a href="https://blog.brod.es/management/2019/07/29/one-on-ones.html">my blog</a> about 1-on-1s and see how I tick.
+        I like programming--- no, seriously, I <em>like</em> programming. I program as a hobby: both after work and on the weekends. I am also passionate about good management; both above me and under me. Read <a href="https://blog.brod.es/management/2019/07/29/one-on-ones.html">my blog</a> about 1-on-1s and see how I tick.
     </p>
 
     <p>If I were asked to “tell me about yourself” in an interview, I would begin with my hometown. I grew up in a small town of 161, Blue Eye, Missouri and it heavily influenced how I see education and learning. I attended the same, small school for 13 years and graduated in its largest-ever class of 76 students.</p>
@@ -14,6 +14,6 @@ export default () => (
       In High School, I became co-owner of Knights of Reason, a gaming community centered around a game Tremulous. Over the next 4 years, I learned the majority of my management and people skills from structuring, discipling, and leading a group of 40 individuals around the globe. I built complex tools in PHP / Bash which assisted in this: KoRPS and KoRMS, which enabled democratic voting in / out of members to our group.
     </p>
 
-      <p>I was fortunate to have discovered Linux in 8th grade. I did not know what to make of it then, but something about it stuck with me. A year later and I had it on my home computer. Fast forward three years and my desktop and laptop both ran Arch Linux, with the latter not even having a Windows install. 10+ years later, all my personal machines still run some variant of Linux as the primary OS.</p>
+      <p>I was fortunate to have discovered Linux in 8th grade. I did not know what to make of it then, but something about it stuck with me. A year later and I had it on my home computer. Fast forward three years and my desktop and laptop both ran Arch Linux, with the latter not even having a Windows install. Years later, all my personal machines still run Linux as the primary OS.</p>
       </React.Fragment>
 );
