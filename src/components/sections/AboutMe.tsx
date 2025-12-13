aea7796bf1c30615bc5939a61ed914fc10b98fac
Billy J Rhoades II <me@brod.es>
2025-12-13 14:27:09 -0800

    Totally rework the opening paragraphs. Ain't no one reading that old crap.

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -3,22 +3,13 @@ import * as React from 'react';
 export default () => (
   <React.Fragment>
     <p>
-      <strong>
-        <a href="mailto:jobs@brod.es">2023: I'm available for hire!</a>
-      </strong>
-      <br />
-      Looking for opportunities, especially early-stage, that align with my personal values.
+      I'm a self-taught systems engineer who learned to code writing RuneScape bots in 8th grade. Growing up in a town of 161 people meant my high school's idea of technology education was a semester of keyboarding.
     </p>
     <p>
-      I'm a software generalist that thrives in ambiguity. I've been fortunate to have roles that consistently reinforce a core belief: software engineering is ultimately a people business. I lean toward growth-minded organizations that put an emphasis on their people. My professional mission is to add business value, foster team communication, provide technical insight, and ultimately enable the business to reach great outcomes.
+      In high school, I inherited a gaming community with 40 members across multiple countries. I learned management by actually doing it: structuring teams, handling conflicts, building democratic voting systems in PHP to keep things running. Most of what I know about leading engineers came from leading gamers who didn't have to show up.
     </p>
-
-    <p>If I were asked to “tell me about yourself” in an interview, I would begin with my hometown. I grew up in a small town of 161, Blue Eye, Missouri and it heavily influenced how I see education and learning. I attended the same, small school for 13 years and graduated in its largest-ever class of 76 students.</p>
-
-    <p>With a small school, I lacked opportunity for a formal introduction into computer science. In 8th grade, I was really into RuneScape but became bored with actually playing it. I joined an online community that wrote bots to play the game for them where I learned my first programming language, Pascal.</p>
-
     <p>
-      In High School, I became co-owner of Knights of Reason, a gaming community centered around a game Tremulous. Over the next 4 years, I learned much of my management fundamentals and people skills from structuring, discipling, and leading a group of 40 diverse individuals from around the globe. I built complex tools in PHP / Bash which assisted in this: KoRPS and KoRMS, which enabled democratic voting of members into our group.
-    </p>
-      </React.Fragment>
+      Software engineering is a people business. The code is the easy part.
+   </p>
+  </React.Fragment>
 );
